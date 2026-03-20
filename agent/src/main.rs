128371cdc738f23fe3ae384de856586789b7a0b5
Göktuğ Türkmen <goktug0306turkmen@gmail.com>
2026-03-20 03:12:41 +0300

    build snapshot and crawl dir

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use core::{compare_snapshots, FileMeta, Snapshot};
+use std::collections::BTreeMap;
+use std::fs;
+use std::path::Path;
+
+const SNAPSHOT_FILE: &str = ".fim_snapshot.json";
+
+fn build_snapshot<P: AsRef<Path>>(dir: P) -> std::io::Result<Snapshot> {
+    let mut snapshot: BTreeMap<String, FileMeta> = Snapshot::new();
+    visit_dirs(dir.as_ref(), &mut snapshot)?;
+    Ok(snapshot)
+}
+
+fn visit_dirs(dir: &Path, snapshot: &mut Snapshot) -> std::io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry: fs::DirEntry = entry?;
+            let path: std::path::PathBuf = entry.path();
+
+            if let Some(file_name) = path.file_name() {
+                if file_name == SNAPSHOT_FILE {
+                    continue;
+                }
+            }
+
+            if path.is_dir() {
+                visit_dirs(&path, snapshot)?;
+            } else {
+                let metadata: fs::Metadata = entry.metadata()?;
+                let size: u64 = metadata.len();
+                let modified: std::time::SystemTime = metadata.modified()?;
+
+                if let Some(path_str) = path.to_str() {
+                    snapshot.insert(
+                        path_str.to_string(),
+                        FileMeta { size, modified },
+                    );
+                }
+            }
+        }
+    }
+    Ok(())
 }
